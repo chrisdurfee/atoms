a18d868ebfdc841173ff23b737b6f1b60140af99
tech_e <chris.durfee@dentalqore.com>
2025-01-17 14:07:52 -0700

    Bump package version to 1.0.41 and update @base-framework/base dependency

--- esbuild.js
+++ esbuild.js
@@ -1,15 +1,15 @@
 import { build } from 'esbuild';
 
 build({
-  entryPoints: ['src/atoms.js'],
-  outdir: 'dist',
-  bundle: true,
-  sourcemap: true,
-  minify: true,
-  splitting: true,
-  treeShaking: true,
-  format: 'esm',  // Output format is ESM
-  target: ['esnext'],
-  external: ['@base-framework/base']
+	entryPoints: ['src/atoms.js'],
+	outdir: 'dist',
+	bundle: true,
+	sourcemap: true,
+	minify: true,
+	splitting: true,
+	treeShaking: true,
+	format: 'esm',  // Output format is ESM
+	target: ['esnext'],
+	external: ['@base-framework/base']
 })
 .catch(() => process.exit(1));
