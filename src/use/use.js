cdf18f93605e7e3864b2ccebfe9e35cf4768313a
tech_e <chris.durfee@dentalqore.com>
2026-03-12 01:01:54 -0600

    Update JSDoc for updateLayout to specify return type as function|undefined

--- src/use/use.js
+++ src/use/use.js
@@ -7,7 +7,7 @@ import { Comment as BaseComment } from "../comment.js";
  * @param {function} callBack
  * @param {object} ele
  * @param {object} parent
- * @returns {function}
+ * @returns {function|undefined}
  */
 const updateLayout = (callBack, ele, parent) =>
 {
